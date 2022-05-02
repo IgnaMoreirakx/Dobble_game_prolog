9ace45fe6d6283040703f4091f405e03220ececa
ignacio Moreira <ignacio.moreira@usach.cl>
2022-05-01 22:14:33 -0400

    Predicados que permiten crear cardset apartir de lista de elementos

--- main.pl
+++ main.pl
@@ -11,38 +11,63 @@
 %totalnncard: umero de elementos por carta, contardor i, contador j contador, K, lista de resultado. La funcion se encarga de crear todas las NN cartas, variando el Ii
 %nncard: encapsula el proceso de creacion de las NN cartas, solo necesita el parametro de numeros de elementos por carta
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Metas 
 %primarias: firstcard y ncard
 
 %secundarias: ncards, cartan, proceso no encapsulado de las N cards
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 %clausuras
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Hechos
-%Ncartas.
+%carta1.
+prueba(Elementos, 0, []).
 carta1(0, []).
+aa(Elementos, 0, [],).
+%Ncartas
+
+prueba2(Elementos, 0, _, _, []).
 ncards(N, 0, J, [1]).
-cartan(N, M, 0, []).
+cartan(Elementos, N, M, 0, []).
 
 %NNcartas.
-firstnncard(N, I, J, 0, [I]).
-secondnncard(N, I, 0, K, []).
-totalnncard(N, 0, J, K, []).
-
+prueba3(Elementos, 0, _, _, _,_, []).
+firstnncard(Elementos, N, I, J, 0, [I]).
+secondnncard(Elementos, N, I, 0, K, []).
+totalnncard(Elementos, N, 0, J, K, []).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Reglas
-%para las Ncartas
+
+%para la primera carta
+prueba(Elementos, N, L):- carta1(N, X), get2(X, Elementos, L).
 carta1(N, [N|L]):- M is N-1, carta1(M, L).
+aa(Elementos, N, [L]):- prueba(Elementos, N, L).
 
-ncard(N,L):- cartan(N,(N-1), (N-1), L).
+%para las Ncartas
 
+ncard(Elementos, N,L):- cartan(Elementos, N,(N-1), (N-1), L).
+prueba2(Elementos, N, M, J, L):- ncards(N, M, J, R), get2(R, Elementos, L). 
 ncards(N, M, J, [K|L]):- T is M-1, K is (N-1)*J + M+1, ncards(N, T, J, L).
-
-cartan(N, M, J, [K|L]):- R is J-1, ncards(N, M, J, K), cartan(N, M, R, L).
+cartan(Elementos, N, M, J, [K|L]):- R is J-1, prueba2(Elementos, N, M, J, K), cartan(Elementos, N, M, R, L).
 
 %Para las NNcartas
-nncard(N, L):- totalnncard(N, (N-1), (N-1), (N-1), X), append(X,L).
 
-firstnncard(N, I, J, K, [X|L]):- T is K -1, X is ((N-1)+2+((N-1)*(K-1)))+((((I-1)*(K-1)+J-1)) mod (N-1)), firstnncard(N, I, J, T,L).
+nncard(Elementos, N, L):- totalnncard(Elementos, N, (N-1), (N-1), (N-1), X), append(X,L).
+prueba3(Elementos, N, I, J, K, A, L):- firstnncard(N,I,J,K,A, X), get2(X, Elementos, L).
 
-secondnncard(N, I, J, K, [X|L]):- R is J-1, firstnncard(N, I, J, K,X), secondnncard(N, I, R, K,L).
+firstnncard(N, I, J, K, A, [X|L]):- E is I+1,T is K -1, X is ((N-1)+2+((N-1)*(K-1)))+((((I-1)*(K-1)+J-1)) mod (N-1)), firstnncard(N, I, J, T, E, L).
 
-totalnncard(N, I, J, K, [Y|Yes]):-  R is I-1, secondnncard(N, I, J, K, Y), totalnncard(N, R, J, K, Yes).
+secondnncard(Elementos, N, I, J, K, [X|L]):- R is J-1, prueba3(Elementos, N, I, J, K, N, X), secondnncard(Elementos, N, I, R, K,L).
+
+totalnncard(Elementos, N, I, J, K, [Y|Yes]):-  R is I-1, secondnncard(Elementos, N, I, J, K, Y), totalnncard(Elementos, N, R, J, K, Yes).
+
+
+%%%%%%%%%%%%%%%%%%%%%funciones auxiliares%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+getElem(1,[C|_],C):-!.
+getElem(X,[_|R],Sol):-X1 is X -1,getElem(X1,R,Sol).
+% get2(Cartas, Elementos, Resultado).
+
+
+get2([], _, []).
+get2([C|Cs], [E|Es], [R|Res]):- getElem(C, [E|Es], R), get2(Cs, [E|Es], Res).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
